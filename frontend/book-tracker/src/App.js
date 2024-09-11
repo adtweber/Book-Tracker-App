a6ea499e112eedcb546a6d09efbc1309d611d1c0
Andi Weber <andiweber@Andis-MacBook-Air.local>
2024-09-11 19:30:29 -0400

    created and exported BookSearch component and connected Google Books API

--- frontend/book-tracker/src/App.js
+++ frontend/book-tracker/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import BookSearch from './components/BookSearch.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Book Tracker</h1>
       <p>Welcome to the book tracker app!</p>
+      <BookSearch />
       {/* Your components will go here */}
     </div>
   );
