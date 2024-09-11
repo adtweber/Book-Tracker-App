ce18597571301a1c0d8695c6cafb32797e58d9e0
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-11 19:19:02 -0400

    added stubs routes, bookTracker and init, as well as .gitignore files

--- Backend/app.py
+++ Backend/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def index(): 
     return 'Hello, this is the Book-Tracker-App'
 
+# books index route
+@app.route('/books')
+def books(): 
+    return 'These are all the books!'
