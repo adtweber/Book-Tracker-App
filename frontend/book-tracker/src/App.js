664f483115ef1aac11c2f6190f26b8dda31186a5
Andi Weber <andiweber@Andis-MacBook-Air.local>
2024-09-11 18:59:03 -0400

    created react app and installed axios

---
+++ frontend/book-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book Tracker</h1>
+      <p>Welcome to the book tracker app!</p>
+      {/* Your components will go here */}
+    </div>
+  );
+}
+
+export default App;
