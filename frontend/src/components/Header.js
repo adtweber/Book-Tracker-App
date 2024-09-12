3560157762ed85128b9c5f0cf33497532d995617
Andi Weber <andiweber@andis-air.lan>
2024-09-12 19:31:31 -0400

    created src folder

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Andi & Judd's Book Tracker</h1>
+        </header>
+    )
+}
+
+export default Header;
