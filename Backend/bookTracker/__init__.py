d0b9c99a28757946be4e9a1a83ab12f729158ef1
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-11 20:32:47 -0400

    connect to postgres database

--- Backend/bookTracker/__init__.py
+++ Backend/bookTracker/__init__.py
@@ -1,7 +1,18 @@
+#config
 from flask import Flask 
+from flask_migrate import Migrate 
 
+#factory
 def create_app(): 
     app = Flask(__name__)
+
+    #database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/Book-Tracker'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello(): 
@@ -18,4 +29,6 @@ def create_app():
 
     # return the app 
     return app
+
+
 
