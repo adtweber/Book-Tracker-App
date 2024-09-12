dddbbbb761c5714ac81a342ff3d5beaabf544aa9
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-11 20:23:49 -0400

    got seed data for users

--- Backend/bookTracker/users.py
+++ Backend/bookTracker/users.py
@@ -1,7 +1,11 @@
 from flask import (Blueprint, render_template)
+import json 
+
+users = json.load(open('users.json'))
+print(users)
 
 bp = Blueprint('users', __name__, url_prefix="/users")
 
 @bp.route('/')
 def index(): 
-    return render_template('index.html')
+    return render_template('index.html',users=users)
