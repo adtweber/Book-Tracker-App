16f79e01ae65530182169070bc6c411788cf8d4d
Andi Weber <andiweber@andis-air.lan>
2024-09-12 20:42:58 -0400

    built BookList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import BookSearch from './components/BookSearch.js'
+import Header from './components/Header.js'
+import BookList from './components/BookList.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Book Tracker</h1>
+      <Header />
       <p>Welcome to the book tracker app!</p>
       <BookSearch />
+      <BookList />
       {/* Your components will go here */}
     </div>
   );
