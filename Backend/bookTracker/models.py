048009388f217d586806ecdcab2c63b8489628ca
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-15 12:50:35 -0400

    added users table with passwords

--- Backend/bookTracker/models.py
+++ Backend/bookTracker/models.py
@@ -13,7 +13,7 @@ class Book(db.Model):
     cover = db.Column(db.String(200), nullable=False)
     status = db.Column(db.String(50), nullable=False, default="Want to Read")
 
-    def __init__(self, title, author, cover, status="Want to Read"):
+    def __init__(self, title, author, cover, status):
         self.title = title
         self.author = author
         self.cover = cover
@@ -28,3 +28,30 @@ class Book(db.Model):
             'cover': self.cover,
             'status': self.status
         }
+
+# Define the User model
+class User(db.Model):
+    __tablename__ = 'User'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password_digest = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(50), nullable=False, default="Want to Read")
+
+    # def __init__(self, title, author, cover, status="Want to Read"):
+    #     self.title = title
+    #     self.author = author
+    #     self.cover = cover
+    #     self.status = status
+
+    # # Method to serialize the model to a dictionary (for JSON responses)
+    # def to_dict(self):
+    #     return {
+    #         'id': self.id,
+    #         'title': self.title,
+    #         'author': self.author,
+    #         'cover': self.cover,
+    #         'status': self.status
+    #     }
