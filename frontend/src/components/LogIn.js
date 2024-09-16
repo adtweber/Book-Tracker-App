f338dee185d0283d67dc33d19f207701c9b94f78
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-16 11:15:52 -0400

    added current user

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { CurrentUser } from "../contexts/CurrentUser";
 
 const Login = () => {
     const [email, setEmail] = useState('')
