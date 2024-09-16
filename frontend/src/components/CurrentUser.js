fa67f572c99e118721436632ae08698745399c45
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-16 10:22:43 -0400

    added userprovder and login.py

---
+++ frontend/src/components/CurrentUser.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+
+export const CurrentUser = createContext()
+
+function CurrentUserProvider({ children }){
+
+    const [currentUser, setCurrentUser] = useState(null)
+
+    //window.setCurrentUser = setCurrentUser;
+    // setCurrentUser({firstName:'Cody',lastName:'Nicholson2'});
+
+    return (
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </CurrentUser.Provider>
+    )
+}
+
+export default CurrentUserProvider
