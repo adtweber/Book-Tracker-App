a541d42efd0f546e4bb462dfbfcfb7f6b21da6df
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-25 01:07:52 -0400

    update authentication controller, included with index, connected to sessions,

--- express_backend/index.js
+++ express_backend/index.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const cookieSession = require('cookie-session')
+
+// Express Settings
+app.use(cookieSession({
+    name: 'session',
+    keys: [ process.env.SESSION_SECRET ],
+    maxAge: 1 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // middleware to parse JSON bodies //
 app.use(express.json());
@@ -12,6 +27,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/books', require('./controllers/books'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // home page //
 app.get('/', (req, res) => {
