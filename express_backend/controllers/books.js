d2c96fdb744e9ea019145e7676cccd6d76611504
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-24 20:12:46 -0400

    added get for all books

--- express_backend/controllers/books.js
+++ express_backend/controllers/books.js
@@ -1,7 +1,17 @@
 // DEPENDENCIES
-const bands = require('express').Router()
+const books = require('express').Router()
 const db = require('../models')
 const { Book } = db 
+
+// FIND ALL BOOKS
+books.get('/', async (req, res) => {
+    try {
+        const foundBooks = await Book.findAll()
+        res.status(200).json(foundBooks)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 // EXPORT
 module.exports = books
