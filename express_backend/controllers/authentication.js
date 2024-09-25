c5a933a6c88c7f7f9c87b9da018e86f2de59bfcd
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-09-25 11:03:55 -0400

    login successfull with jwt

--- express_backend/controllers/authentication.js
+++ express_backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token');
 
 const { User } = db
 
@@ -10,18 +11,27 @@ router.post('/', async (req, res) => {
             where: { email: req.body.email }
         });
 
-        if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        if (!user || !await bcrypt.compare(req.body.password, user.password_digest)) {
             return res.status(404).json({ 
                 message: 'Could not find a user with the provided username and password' 
             });
+        } else {
+            jwt.encode(process.env.JWT_SECRET, { id: user.userId }, (err, token) => {
+                if (err) {
+                    return res.status(500).json({ message: 'Error generating token' });
+                }
+                
+                console.log('Token generated:', token);  // Log token after creation
+                return res.json({ user: user, token: token });
+            });
         }
-
+        // console.log('Token generated:', result); 
         // If the user is authenticated
-        req.session.userId = user.userId;
-        return res.json({ user });
+        // req.session.userId = user.userId;
+        // return res.json({ user });
     } catch (error) {
         // Handle any unexpected errors
-        console.error('Error:', error);
+        console.error('Server Error:', error.stack);
         return res.status(500).json({ message: 'Internal server error' });
     }
 });
